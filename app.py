28fec81a01538c832c2323af0b8a15bb4bf55f60
muhamadirpan266 <fotopribadi56@gmail.com>
2026-04-21 08:35:38 +0700

    innitial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+for i in range(4):
+    for x in range(6,-1,-1):
+        print(x, end=" " )
+    print()
+    
+for i in range(3):
+    for x in range(5,-1,-1):
+        print(x, end=" ")
+    print()
+
+for i in range(5,2,-1):
+    for x in range(7):
+        print(i, end=" ")
+    print()
+
+for i in range(5) :
+    for x in range(7):
+        print(i+1, end=" ")
+    print()
+
+for c in range(1,6,+2) :
+    for d in range(7):
+        print(c+1, end=" ")
+    print()
+
+for b in range(4, 1) :
+    for v in range(7):
+        print(b+1, end=" ")
+    print()
